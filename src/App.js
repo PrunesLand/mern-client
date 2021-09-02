1fb60567eda45420f7b700ef9f78bc57e84ec96d
prunesland <panargya@gmail.com>
2021-09-02 19:13:23 +0700

    creating landing page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Auth from './components/Auth/Auth';
+import LandingPage from './components/landingPage/LandingPage';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Container>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path='/' exact component={LandingPage} />
           <Route path='/auth' exact component={Auth} />
         </Switch>
       </Container>
